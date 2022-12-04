3bb7b26a59a6236d95ac5cb85f7b9a12cb9c09b6
immurried <zackdingwall@gmail.com>
2022-12-05 09:42:56 +1100

    day four solution

---
+++ DayFour/DayFour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class DayFour : Puzzle
+{
+    public DayFour(string file) : base(file)
+    {
+    }
+
+    public override string Compute()
+    {
+        base.Compute();
+
+        int countOverlaps = 0;
+
+        foreach (string line in input)
+        {
+            string[] ranges = line.Split(',');
+            int a = Int32.Parse(ranges[0].Split('-')[0]);
+            int b = Int32.Parse(ranges[0].Split('-')[1]);
+            int x = Int32.Parse(ranges[1].Split('-')[0]);
+            int y = Int32.Parse(ranges[1].Split('-')[1]);
+
+            //if (RangeContainsRange(a,b,x,y) || RangeContainsRange(x,y,a,b))
+            if (RangesOverlap(a,b,x,y))
+                countOverlaps++;
+        }
+
+        return countOverlaps.ToString();
+    }
+
+    private bool RangeContainsRange(int a, int b, int x, int y)
+    {
+        return (x >= a && y <= b);
+    }
+
+    private bool RangesOverlap(int a, int b, int x, int y)
+    {
+        return (x >= a && x <= b) || (y >= a && y <= b) ||
+            (a >= x && a <= y) || (b >= x && b <= y);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ List<Puzzle> puzzles = new List<Puzzle>();
 puzzles.Add(new DayOne("d1_input.txt"));
 puzzles.Add(new DayTwo("d2_input.txt"));
 puzzles.Add(new DayThree("d3_input.txt"));
+puzzles.Add(new DayFour("d4_input.txt"));
 
 int count = 1;
 foreach (Puzzle puzzle in puzzles)
